fcb48c1bfa03dc24882f6ce2271f98eb912b249a
GustavoSantos25 <gustavo.war2015@gmail.com>
2022-06-15 14:39:40 -0300

    Adiciona arquivo temporário para aulas (bd-doc)

---
+++ aula-bd-doc.js
@@ -0,0 +1,245 @@
+//OFERECIMENTO 1
+{
+    "sala":"10A",
+    "status":"completa",
+    "horario_inicio":new Date("2022-03-23T10:00"),
+    "horario_fim":new Date("2022-03-23T11:40"),
+    "usuarios":[11225698, 89754613, 78124549]
+}
+
+{
+    "sala":"10A",
+    "status":"completa",
+    "horario_inicio":new Date("2022-03-25T08:00"),
+    "horario_fim":new Date("2022-03-25T09:40"),
+    "usuarios":[11225698, 89754613, 78124549]
+}
+
+{
+    "sala":"10A",
+    "status":"completa",
+    "horario_inicio":new Date("2022-03-30T10:00"),
+    "horario_fim":new Date("2022-03-30T11:40"),
+    "usuarios":[11225698, 89754613, 78124549]
+}
+
+{
+    "sala":"10A",
+    "status":"completa",
+    "horario_inicio":new Date("2022-04-01T08:00"),
+    "horario_fim":new Date("2022-04-01T09:40"),
+    "usuarios":[11225698, 89754613, 78124549]
+}
+
+{
+    "sala":"10A",
+    "status":"completa",
+    "horario_inicio":new Date("2022-04-06T10:00"),
+    "horario_fim":new Date("2022-04-06T11:40"),
+    "midias":["M9"],
+    "usuarios":[11225698, 89754613, 78124549]
+}
+
+{
+    "sala":"10A",
+    "status":"completa",
+    "horario_inicio":new Date("2022-04-08T08:00"),
+    "horario_fim":new Date("2022-04-08T09:40"),
+    "midias":["M0"],
+    "usuarios":[11225698, 89754613, 78124549]
+}
+
+
+//OFERECIMENTO 2
+{
+    "sala":"13B",
+    "status":"completa",
+    "horario_inicio":new Date("2022-04-04T08:00"),
+    "horario_fim":new Date("2022-04-04T09:40"),
+    "usuarios":[12161816, 23456789, 78124549]
+}
+
+{
+    "sala":"13B",
+    "status":"completa",
+    "horario_inicio":new Date("2022-04-06T10:00"),
+    "horario_fim":new Date("2022-04-06T11:40"),
+    "midias":["M4"],
+    "usuarios":[12161816, 23456789]
+}
+
+{
+    "sala":"13B",
+    "status":"completa",
+    "horario_inicio":new Date("2022-04-11T08:00"),
+    "horario_fim":new Date("2022-04-11T09:40"),
+    "usuarios":[12161816, 23456789]
+}
+
+{
+    "sala":"13B",
+    "status":"completa",
+    "horario_inicio":new Date("2022-04-13T10:00"),
+    "horario_fim":new Date("2022-04-13T11:40"),
+    "usuarios":[12161816, 23456789, 78124549]
+}
+
+{
+    "sala":"13B",
+    "status":"agendada",
+    "horario_inicio":new Date("2022-07-11T08:00"),
+    "horario_fim":new Date("2022-07-11T09:40"),
+    "usuarios":[12161816, 23456789]
+}
+
+{
+    "sala":"13B",
+    "status":"agendada",
+    "horario_inicio":new Date("2022-07-13T10:00"),
+    "horario_fim":new Date("2022-07-13T11:40"),
+    "usuarios":[12161816, 23456789, 78124549]
+}
+
+
+//OFERECIMENTO 3
+{
+    "sala":"11A",
+    "status":"agendada",
+    "horario_inicio":new Date("2022-06-21T14:00"),
+    "horario_fim":new Date("2022-06-21T17:00"),
+    "midias":["M1"],
+    "usuarios":[785461, 12341516, 19756484]
+}
+
+
+{
+    "sala":"11A",
+    "status":"agendada",
+    "horario_inicio":new Date("2022-06-28T14:00"),
+    "horario_fim":new Date("2022-06-28T17:00"),
+    "midias":["M2"],
+    "usuarios":[785461, 12341516, 19756484]
+}
+
+{
+    "sala":"11A",
+    "status":"agendada",
+    "horario_inicio":new Date("2022-07-05T14:00"),
+    "horario_fim":new Date("2022-07-05T17:00"),
+    "usuarios":[785461, 12341516, 19756484]
+}
+
+{
+    "sala":"11A",
+    "status":"agendada",
+    "horario_inicio":new Date("2022-07-12T14:00"),
+    "horario_fim":new Date("2022-07-12T17:00"),
+    "usuarios":[12341516, 19756484]
+}
+
+
+//OFERECIMENTO 4
+{
+    "sala":"113A",
+    "status":"cancelada",
+    "horario_inicio":new Date("2022-06-21T08:00"),
+    "horario_fim":new Date("2022-06-21T09:40"),
+    "usuarios":[11256973, 13125467, 12486789, 13469284]
+}
+
+
+{
+    "sala":"113A",
+    "status":"cancelada",
+    "horario_inicio":new Date("2022-06-2310:00"),
+    "horario_fim":new Date("2022-06-23T11:40"),
+    "usuarios":[11256973, 13125467, 12486789, 13469284]
+}
+
+{
+    "sala":"113A",
+    "status":"completa",
+    "horario_inicio":new Date("2022-06-09T10:00"),
+    "horario_fim":new Date("2022-06-09T11:40"),
+    "midias":["M7"],
+    "usuarios":[11256973, 13125467, 12486789, 13469284]
+}
+
+{
+    "sala":"113A",
+    "status":"completa",
+    "horario_inicio":new Date("2022-06-07T08:00"),
+    "horario_fim":new Date("2022-06-07T09:40"),
+    "usuarios":[11256973, 13125467, 12486789, 13469284]
+}
+
+{
+    "sala":"113A",
+    "status":"completa",
+    "horario_inicio":new Date("2022-06-02T10:00"),
+    "horario_fim":new Date("2022-06-02T11:40"),
+    "usuarios":[11256973, 13125467, 12486789, 13469284]
+}
+
+{
+    "sala":"113A",
+    "status":"completa",
+    "horario_inicio":new Date("2022-05-31T10:00"),
+    "horario_fim":new Date("2022-05-31T11:40"),
+    "usuarios":[11256973, 13125467, 13469284]
+}
+
+
+//OFERECIMENTO 5
+{
+    "sala":"09C",
+    "status":"completa",
+    "horario_inicio":new Date("2022-05-23T14:00"),
+    "horario_fim":new Date("2022-05-23T15:40"),
+    "usuarios":[16327498, 8754216, 7524315]
+}
+
+
+{
+    "sala":"09C",
+    "status":"completa",
+    "horario_inicio":new Date("2022-05-25T16:00"),
+    "horario_fim":new Date("2022-05-25T17:40"),
+    "midias":["M8"],
+    "usuarios":[16327498, 8754216, 7524315]
+}
+
+{
+    "sala":"09C",
+    "status":"completa",
+    "horario_inicio":new Date("2022-05-30T14:00"),
+    "horario_fim":new Date("2022-05-30T15:40"),
+    "usuarios":[16327498, 8754216, 7524315]
+}
+
+{
+    "sala":"09C",
+    "status":"completa",
+    "horario_inicio":new Date("2022-06-01T16:00"),
+    "horario_fim":new Date("2022-06-01T17:40"),
+    "usuarios":[16327498, 8754216, 7524315]
+}
+
+{
+    "sala":"09C",
+    "status":"agendada",
+    "horario_inicio":new Date("2022-07-04T14:00"),
+    "horario_fim":new Date("2022-05-30T15:40"),
+    "usuarios":[16327498, 8754216, 7524315]
+}
+
+{
+    "sala":"09C",
+    "status":"agendada",
+    "horario_inicio":new Date("2022-07-06T16:00"),
+    "horario_fim":new Date("2022-06-01T17:40"),
+    "usuarios":[16327498, 8754216, 7524315]
+}
+
+
+

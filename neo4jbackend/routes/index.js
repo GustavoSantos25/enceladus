0a6142233a564db14ff6d5aefd52dc0f4de18870
iangalvao <iansgalvao@gmail.com>
2022-07-21 15:55:51 -0300

    arquivos do exemplo deletados

--- neo4jbackend/routes/index.js
+++ neo4jbackend/routes/index.js
@@ -1,8 +1,5 @@
 // convenience wrapper around all other files:
 exports.users = require('./users');
 exports.site = require('./site');
-exports.people = require('./people');
-exports.movies = require('./movies');
-exports.genres = require('./genres');
 exports.professores = require('./professores');
 exports.usuarios = require('./usuarios');
